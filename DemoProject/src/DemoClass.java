29508b9f160901785044a3d751b0a8250ccad8f0
kpnishanth <nishanth.prabhakaran@gmail.com>
2017-10-14 22:00:13 -0500

    Adding sum function

--- DemoProject/src/DemoClass.java
+++ DemoProject/src/DemoClass.java
@@ -3,7 +3,16 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int a=0, b=0;
 		System.out.println("New Test Project");
+		sum(467,6776);
+		
+	}
+
+	private static void sum(int a, int b) {
+		// TODO Auto-generated method stub
+		System.out.println(("sum of two numbers   "+(a+b)));
 	}
 
 }
