3dce2e677d6699ce57f242237e2ad7057f0fb2d3
kpnishanth <nishanth.prabhakaran@gmail.com>
2017-10-14 22:01:15 -0500

    Changes suggested by CEO incorporated

--- DemoProject/src/DemoClass.java
+++ DemoProject/src/DemoClass.java
@@ -3,7 +3,7 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("New Test Project");
+		System.out.println("New Test Project with changes suggested by CEO");
 	}
 
 }
